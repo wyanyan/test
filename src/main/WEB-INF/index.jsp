c27c53f6a3774219d74ddce48e034092cd1c025c
wyanyan <1026730298@qq.com>
2017-06-14 11:18:17 +0800

    Initial commit

---
+++ src/main/WEB-INF/index.jsp
@@ -0,0 +1,7 @@
+<%@ page contentType="text/html; charset=UTF-8"%>
+<html>
+    <head></head>
+    <body>
+        <div>hello word</div>
+    </body>
+</html>
---
+++ src/main/java/com/zdwx/test/Test.java
@@ -0,0 +1,12 @@
+package com.zdwx.test;
+
+/**
+ * Created by wyy on 2017/6/8.
+ */
+public class Test
+{
+    //TODO
+    public static void main(String[] args){
+        System.out.println("hello word");
+    }
+}
